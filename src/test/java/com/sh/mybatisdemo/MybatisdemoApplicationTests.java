88d8d812bb3937374c275cd33aa41a5e512e1edb
zangaihu <1125245923@qq.com>
2020-05-20 08:49:07 +0800

    Initial commit

---
+++ src/test/java/com/sh/mybatisdemo/MybatisdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.mybatisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
